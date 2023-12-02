f080bf02976afd65662d37a179cd42ffb3f7600a
Hania Imran <hanna.khan205@gmail.com>
2023-12-02 13:14:17 +0500

    Feature component fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import './App.css';
 import Hero from './components/Hero';
-import About from './components/About';
 import Feature from './components/Feature';
+import About from './components/About';
+
 
 function App() {
   return (
