37142980f8dcdda583d432c62652227ffbc784e7
Talha Haider <109687407+talha2224@users.noreply.github.com>
2023-12-03 16:23:17 +0500

    ServiceOfferings Responsive done

--- src/components/ServiceOfferings.jsx
+++ src/components/ServiceOfferings.jsx
@@ -15,15 +15,16 @@ const ServiceOfferings = () => {
     <div className=" relative top-[55vh] w-screen flex justify-center items-center flex-col font-poppins">
 
       {/* UPPER BOX  */}
-      <div className="flex justify-between items-center w-[100%] h-fit bg-[#F8F8F8]">
+      <div className="md:flex justify-between items-center w-[100%] h-fit bg-[#F8F8F8]">
 
-        <div className="flex-1 pl-[10rem]">
+        <div className="lg:pl-[2rem] pl-[1rem] flex-1 pt-[8rem] pb-[8rem] md:pt-0 md:pb-0">
           <h1 className="text-2xl mb-2 font-semibold text-[#F16427]">Services</h1>
           <h1 className="text-3xl mb-2 font-semibold text-[#FDCE0C]">Offerings</h1>
+          <p className="text-xs font-semibold text-[#283D4B] w-[90%]">Are you tired of going to service stations only to discover that they are all full and having to return without getting your services?Tajammul.pk saves the day by allowing you to pre-book your services based on their availability. We keep an updated calendar so that we can provide you with the best possible services. You can now book a time slot and get your services. We offer salon, clinical, catering, decoration, auto mechanic, and plumbing services, as well as the option of hiring labor or maids for home services. Save yourself hours of waiting in lines by scheduling your services according to your availability.</p>
           <p className="text-sm font-semibold text-[#283D4B] w-[80%]">Are you tired of going to service stations only to discover that they are all full and having to return without getting your services?Tajammul.app saves the day by allowing you to pre-book your services based on their availability. We keep an updated calendar so that we can provide you with the best possible services. You can now book a time slot and get your services. We offer salon, clinical, catering, decoration, auto mechanic, and plumbing services, as well as the option of hiring labor or maids for home services. Save yourself hours of waiting in lines by scheduling your services according to your availability.</p>
         </div>
 
-        <div className="flex-1 relative flex justify-center items-center bg-gradient-to-br from-blue-800 to-blue-900 h-[25rem] pt-3">
+        <div className="flex-1 relative flex justify-center  items-center bg-gradient-to-br from-blue-800 to-blue-900 h-[25rem] pt-3">
           <img src={RLImage} alt="" className="h-[15rem] mr-[-2rem]" />
           <img src={RICenter} alt="" className="h-[20rem] z-50" />
           <img src={RImage} alt="" className="h-[15rem] ml-[-1.7rem] z-50" />
@@ -34,20 +35,26 @@ const ServiceOfferings = () => {
       </div>
 
       {/* LOWER BOX  */}
-      <div className="flex justify-between items-center w-[100%] h-fit bg-[#F8F8F8]">
+      <div className="md:flex justify-between items-center w-[100%] h-fit bg-[#F8F8F8]">
 
-        <div className="w-[55.88%] relative flex justify-center items-center bg-gradient-to-br from-blue-800 to-blue-900 h-[25rem] pt-3">
+        <div className="flex-1 hidden  relative md:flex justify-center items-center bg-gradient-to-br from-blue-800 to-blue-900 h-[25rem] pt-3">
           <img src={RLImage} alt="" className="h-[15rem] mr-[-2rem]" />
           <img src={Center2Image} alt="" className="h-[20rem] z-50" />
           <img src={VegImage} alt="" className="h-[15rem] ml-[-1.7rem] z-50" />
           <img className=" absolute left-0 top-[0%] h-[7rem]" src={Decorter2} alt="" srcset="" />
         </div>
 
-        <div className="flex-1 pl-[3rem] relative">
+        <div className="flex-1 pl-[3rem] relative pt-[8rem] pb-[8rem] md:pt-0 md:pb-0">
           <h1 className="text-2xl mb-2 font-semibold text-[#F16427]">Product</h1>
           <h1 className="text-3xl mb-2 font-semibold text-[#FDCE0C]">Offerings</h1>
-          <p className="text-sm font-semibold text-[#283D4B] w-[80%]">Tajammul offers a wide range of products to satisfy your cravings, meet your demands, fulfill your desires and, most importantly, make your life easier. It includes everything from groceries to cuisine that is store-friendly. Snacks, fruits and vegetables, meat, milk, drinks and beverages, chocolates and sweets, household needs, party supplies, cosmetics, baby essentials, urgent pharmaceutical deliveries, frozen foods, and much more are all included in this product category. Also, now you can scroll through a variety of clothes and purchase what suits you best with just a few clicks away. </p>
+          <p className="text-xs font-semibold text-[#283D4B] w-[80%]">Tajammul offers a wide range of products to satisfy your cravings, meet your demands, fulfill your desires and, most importantly, make your life easier. It includes everything from groceries to cuisine that is store-friendly. Snacks, fruits and vegetables, meat, milk, drinks and beverages, chocolates and sweets, household needs, party supplies, cosmetics, baby essentials, urgent pharmaceutical deliveries, frozen foods, and much more are all included in this product category. Also, now you can scroll through a variety of clothes and purchase what suits you best with just a few clicks away. </p>
           <img className="absolute right-0 top-[45%] h-[13rem]" src={Decorter3} alt="image" srcset="" />
+        </div>
+        <div className="flex  relative md:hidden justify-center items-center bg-gradient-to-br from-blue-800 to-blue-900 h-[25rem] pt-3">
+          <img src={RLImage} alt="" className="h-[15rem] mr-[-2rem]" />
+          <img src={Center2Image} alt="" className="h-[20rem] z-50" />
+          <img src={VegImage} alt="" className="h-[15rem] ml-[-1.7rem] z-50" />
+          <img className=" absolute left-0 top-[0%] h-[7rem]" src={Decorter2} alt="" srcset="" />
         </div>
 
 
