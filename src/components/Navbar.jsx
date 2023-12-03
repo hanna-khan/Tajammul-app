c52b9574caa0bd03448ef7a3a4cfebc8552821e1
Talha Haider <109687407+talha2224@users.noreply.github.com>
2023-12-03 16:25:17 +0500

    Navbar Responsive Done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Logo from '../assests/tajamul.png'
 import HalfCircle from '../assests/navCircle.png'
 import Circle from '../assests/cicle.png'
 import { FaFacebookF, FaInstagram, FaTwitter, FaLinkedinIn } from "react-icons/fa";
+import { GiHamburgerMenu } from 'react-icons/gi'
 import { navLinks } from '../constants/navData';
 
 const Navbar = () => {
+
+    const [showNavLinks, setshowNavLinks] = useState(false)
     return (
 
         <div className='w-screen  pb-10 relative'>
@@ -14,32 +17,48 @@ const Navbar = () => {
             <div className='flex absolute top-[2rem] justify-center items-center w-[100%] h-fit z-50'>
 
                 {/* MAIN NAVBAR  */}
-                <div className='w-[80%] h-[100%] shadow-navbarShadow bg-white flex justify-between items-center pl-2 pr-2'>
+                <div className='w-[90%] lg:w-[85%] h-[100%] shadow-navbarShadow bg-white flex justify-between items-center pl-2 pr-2 relative'>
 
                     {/* LOGO SECTION  */}
                     <div>
-                        <img src={Logo} alt="" className='h-[3rem]' />
+                        <img src={Logo} alt="" className='lg:h-[3rem] h-[2rem]' />
                     </div>
 
                     {/* NAVLINKS SECTION  */}
-
-                    <div className='flex gap-x-3 items-center'>
+                    <div className='sm:flex hidden gap-x-3 items-center p-0 bg-none '>
                         {
                             navLinks?.map((item, index) => {
                                 return (
-                                    <p key={index + 1} className='text-[#313131] font-poppins cursor-pointer'>{item?.title}</p>
+                                    <p key={index + 1} className='text-[#313131] text-sm lg:text-base font-poppins cursor-pointer'>{item?.title}</p>
                                 )
                             })
                         }
                     </div>
 
+                    {/* RESPONSIVE NAVLINK SECTION  */}
+                    {
+                        showNavLinks && (
+                            <div className='absolute right-0 top-[2rem] block sm:hidden w-[100%] pt-5 pb-5 pl-3 bg-white '>
+                                {
+                                    navLinks?.map((item, index) => {
+                                        return (
+                                            <p key={index + 1} className='text-[#313131] mb-2 text-sm font-poppins cursor-pointer'>{item?.title}</p>
+                                        )
+                                    })
+                                }
+                            </div>
+
+                        )
+                    }
+
                     {/* SOCIALLINKS SECTION  */}
 
                     <div className='flex gap-x-3 items-center'>
-                        <FaFacebookF className='text-[#F16427] text-lg' />
-                        <FaInstagram className='text-[#F16427] text-lg' />
-                        <FaTwitter className='text-[#F16427] text-lg' />
-                        <FaLinkedinIn className='text-[#F16427] text-lg' />
+                        <FaFacebookF className='text-[#F16427] text-sm lg:text-lg' />
+                        <FaInstagram className='text-[#F16427] text-sm lg:text-lg' />
+                        <FaTwitter className='text-[#F16427] text-sm lg:text-lg' />
+                        <FaLinkedinIn className='text-[#F16427] text-sm lg:text-lg' />
+                        <GiHamburgerMenu onClick={() => setshowNavLinks(!showNavLinks)} className='text-[#F16427] text-sm lg:text-lg block sm:hidden' />
                     </div>
 
                 </div>
@@ -50,13 +69,15 @@ const Navbar = () => {
 
             <div>
                 {/* RIGHT CIRCLE  */}
-                <div className=' absolute right-0 top-0'>
-                    <img src={Circle} alt="" className='h-[13rem]' />
+                <div className='absolute right-0 top-0'>
+                    <img src={Circle} alt="" className='md:h-[10rem] h-[6rem]' />
                 </div>
+
                 {/* LEFT CIRCLE  */}
-                <div className=' absolute left-0 top-0'>
-                    <img src={HalfCircle} alt="" className='h-[13rem]' />
+                <div className='absolute left-0 top-0'>
+                    <img src={HalfCircle} alt="" className='md:h-[13rem] h-[7rem]' />
                 </div>
+
             </div>
 
 
@@ -65,3 +86,4 @@ const Navbar = () => {
 }
 
 export default Navbar
+
